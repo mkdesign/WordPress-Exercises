677adbd084943d692bef2ca5fcf74d10d89755dc
mehrad <mehrad.kavian@gmail.com>
2012-09-03 02:20:32 +0430

    starting to work on wordpress

---
+++ inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="style.css" />
+	<title>My Weblog</title>
+</head>
+<body>
+
+<div class="container">
+  <div class="header"><a href="#"><img src="" alt="Insert Logo Here" name="Insert_logo" width="180" height="90" id="Insert_logo" style="background: #C6D580; display:block;" /></a> 
+    <!-- end .header --></div>
