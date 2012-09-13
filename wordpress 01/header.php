2dc8d856189e601d84b86bcc9723205c47d3ff89
mehrad <mehrad.kavian@gmail.com>
2012-09-13 19:03:36 +0430

    seperated parts

---
+++ wordpress 01/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="style.css" />
+	<title>test2</title>
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header"><a href="#"><img src="" alt="Insert Logo Here" name="Insert_logo" width="180" height="90" id="Insert_logo" style="background: #C6D580; display:block;" /></a> 
+    <!-- end .header --></div>
