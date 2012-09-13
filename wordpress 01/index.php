0b5c92b2aea658b0189f656b73d6c37a78d6d07a
mehrad <mehrad.kavian@gmail.com>
2012-09-13 21:01:13 +0430

    adding other parts

--- wordpress 01/index.php
+++ wordpress 01/index.php
@@ -1,4 +1,6 @@
 <?php
 	get_template_part('header');
-	get_template_part('sidbar-left');
+	get_template_part('sidebar-left');
 	get_template_part('content');
+	get_template_part('sidebar-right');
+	get_template_part('footer');
